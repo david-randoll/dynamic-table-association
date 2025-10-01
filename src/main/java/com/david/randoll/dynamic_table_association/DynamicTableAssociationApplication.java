801715ba765032afa406ba4b71b7b770ba0eae23
d-randoll <51640895+d-randoll@users.noreply.github.com>
2025-10-01 08:53:13 -0400

    Initial commit

---
+++ src/main/java/com/david/randoll/dynamic_table_association/DynamicTableAssociationApplication.java
@@ -0,0 +1,13 @@
+package com.david.randoll.dynamic_table_association;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicTableAssociationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicTableAssociationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/randoll/dynamic_table_association/DynamicTableAssociationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.randoll.dynamic_table_association;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicTableAssociationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
